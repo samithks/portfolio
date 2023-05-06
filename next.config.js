a483cf3ab3eb8aa8378e27e135cafd61a5d837b1
Samith K S <samithks@users.noreply.github.com>
2023-05-06 19:47:07 +0530

    feat: enable scss
    to support scss

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+import { join } from 'node:path'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [join(__dirname, 'styles')],
+  },
+}
 
 module.exports = nextConfig
