f1f0d2f5b819d55259b23dacb8fa522a4b52f8ce
Samith K S <samithks@users.noreply.github.com>
2023-05-08 14:45:09 +0530

    feat: content layer
    content layer added for blog

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-const { join } =  require('node:path')
+const { withContentlayer } = require("next-contentlayer")
+const { join } = require('node:path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   sassOptions: {
     includePaths: [join(__dirname, 'styles')],
   },
 }
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig)
