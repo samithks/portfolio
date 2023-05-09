6b82a35afbb56c39e8be649f11d01303f62e84ee
Samith K S <samithks@users.noreply.github.com>
2023-05-09 09:28:40 +0530

    feat: security header
    security header

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
-const { withContentlayer } = require("next-contentlayer")
 const { join } = require('node:path')
+const { withContentlayer } = require('next-contentlayer')
+const  securityHeaders = require('./security-headers')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
