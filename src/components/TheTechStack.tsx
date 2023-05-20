3160ac314c37fa71929f952b9157ae2b36b5fbb8
Samith K S <samithks@users.noreply.github.com>
2023-05-20 21:21:17 +0530

    feat: Techstack
    teckstack page

--- src/components/TheTechStack.tsx
+++ src/components/TheTechStack.tsx
@@ -1,127 +1,185 @@
+import { BrandIcon } from '@/components/icon/BrandIcon'
+
 interface ITechnology {
   title: string
   icon: string
 }
 
 interface ITechStack {
-  development: ITechnology[]
+  languages: ITechnology[]
   backend: ITechnology[]
   frontend: ITechnology[]
-  tools: ITechnology[]
+  datastore: ITechnology[]
   platform: ITechnology[]
+  tools: ITechnology[]
 }
 
 const techStack: ITechStack = {
-  development: [
+  languages: [
     {
-      title: 'Java',
-      icon: 'mdi-language-java',
+      title: 'Typescript',
+      icon: 'typescript',
+    },
+    {
+      title: 'JavaScript',
+      icon: 'javascript',
+    },
+    {
+      title: 'HTML',
+      icon: 'html',
+    },
+    {
+      title: 'CSS',
+      icon: 'css',
     },
     {
       title: 'Python',
-      icon: 'mdi-language-python',
+      icon: 'python',
     },
     {
-      title: 'C#',
-      icon: 'mdi-language-csharp',
-    },
-    {
-      title: 'C++',
-      icon: 'mdi-language-cpp',
-    },
-    {
-      title: 'C',
-      icon: 'mdi-language-c',
+      title: 'Sass',
+      icon: 'sass',
     },
   ],
   backend: [
     {
       title: 'NodeJS',
-      icon: 'mdi-nodejs',
+      icon: 'nodejs',
     },
     {
-      title: 'ExpressJS',
-      icon: 'mdi-nodejs',
+      title: 'Express',
+      icon: 'express',
     },
     {
-      title: 'Spring Boot',
-      icon: 'mdi-spring',
+      title: 'NestJs',
+      icon: 'nestjs',
     },
     {
-      title: 'Flask',
-      icon: 'mdi-flask',
+      title: 'Loopback',
+      icon: 'loopback',
     },
     {
-      title: 'Django',
-      icon: 'mdi-django',
+      title: 'TypeGraphQL',
+      icon: 'graphql',
     },
   ],
   frontend: [
     {
       title: 'VueJS',
-      icon: 'mdi-vuejs',
+      icon: 'vue',
     },
     {
       title: 'ReactJS',
-      icon: 'mdi-react',
+      icon: 'react',
     },
     {
-      title: 'Angular',
-      icon: 'mdi-angular',
+      title: 'NextJS',
+      icon: 'nextjs',
     },
     {
-      title: 'HTML',
-      icon: 'mdi-language-html5',
+      title: 'NuxtJS',
+      icon: 'nuxtjs',
     },
     {
-      title: 'CSS',
-      icon: 'mdi-language-css3',
+      title: 'TailwindCSS',
+      icon: 'tailwindcss',
     },
   ],
-  tools: [
+  datastore: [
     {
-      title: 'Git',
-      icon: 'mdi-git',
+      title: 'MongoDB',
+      icon: 'mongodb',
     },
     {
-      title: 'GitHub',
-      icon: 'mdi-github',
+      title: 'MySQL',
+      icon: 'mysql',
     },
     {
-      title: 'GitLab',
-      icon: 'mdi-gitlab',
+      title: 'PostgreSQL',
+      icon: 'postgresql',
     },
     {
-      title: 'Docker',
-      icon: 'mdi-docker',
+      title: 'Redis',
+      icon: 'redis',
     },
     {
-      title: 'Kubernetes',
-      icon: 'mdi-kubernetes',
+      title: 'InfluxDB',
+      icon: 'influxdb',
     },
   ],
   platform: [
     {
       title: 'AWS',
-      icon: 'mdi-aws',
+      icon: 'aws',
     },
     {
       title: 'Azure',
-      icon: 'mdi-microsoft-azure',
-    },
-    {
-      title: 'Google Cloud',
-      icon: 'mdi-google-cloud',
+      icon: 'azure',
     },
     {
       title: 'Heroku',
-      icon: 'mdi-heroku',
+      icon: 'heroku',
     },
     {
-      title: 'Firebase',
-      icon: 'mdi-firebase',
+      title: 'Github',
+      icon: 'github',
+    },
+    {
+      title: 'Bitbucket',
+      icon: 'bitbucket',
+    },
+    {
+      title: 'Gitlab',
+      icon: 'gitlab',
     },
   ],
+  tools: [
+    {
+      title: 'Hasura',
+      icon: 'hasura',
+    },
+    {
+      title: 'GraphQL',
+      icon: 'graphql',
+    },
+    {
+      title: 'Keycloak',
+      icon: 'keycloak',
+    },
+    {
+      title: 'SSO',
+      icon: 'sso',
+    },
+    {
+      title: 'CubeJS',
+      icon: 'cubejs',
+    },
+    {
+      title: 'Auth0',
+      icon: 'auth0',
+    },
+    {
+      title: 'LaunchDarkly',
+      icon: 'launchdarkly',
+    },
+    {
+      title: 'Docker',
+      icon: 'docker',
+    },
+  ],
+}
+
+const TechCard: React.FC<ITechnology> = ({ title, icon }) => {
+  return (
+    <div className="block space-y-2 rounded-md border border-slate-200 bg-white p-6 pt-5 shadow-md transition duration-300 hover:bg-slate-100 hover:shadow-lg dark:border-gray-200 dark:bg-black/5 dark:shadow-white/10 dark:hover:bg-white/10 dark:hover:shadow-lg dark:hover:shadow-white/20">
+      <div className="flex flex-row gap-x-2">
+        <BrandIcon name={icon} fontSize={'2em'} className="stroke-2" />
+        <h2 className="text-md mb-2 truncate font-medium leading-snug tracking-tight text-black dark:text-white">
+          {title}
+        </h2>
+      </div>
+    </div>
+  )
 }
 
 const TheTechStack: React.FC = () => {
@@ -129,6 +187,25 @@ const TheTechStack: React.FC = () => {
     <section id="tech_stack" className="mx-5 flex flex-col gap-y-3 px-5">
       <div className="">
         <h2 className="text-sm font-medium uppercase text-white/60">Tech Stack</h2>
+      </div>
+
+      <div className="mt-4 flex flex-col gap-x-5 gap-y-10">
+        {Object.keys(techStack).map((key) => {
+          return (
+            <div className="flex-1" key={key}>
+              <h3 className="text-sm font-semibold text-white/80">{key}</h3>
+              <div className="mt-3 grid grid-cols-4 gap-3">
+                {techStack[key as keyof ITechStack].map((tech) => {
+                  return (
+                    <div className="col-span-1" key={tech.title}>
+                      <TechCard title={tech.title} icon={tech.icon} />
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          )
+        })}
       </div>
     </section>
   )
