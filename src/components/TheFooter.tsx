02313a490fa84233aa3d63879b714255d3244d1e
Samith K S <samithks@users.noreply.github.com>
2023-05-21 14:36:34 +0530

    fix: jsx mac depth
    jsx max depth

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -10,15 +10,23 @@ const footerReference: UrlObject = {
   hostname: DOMAIN_NAME,
 }
 
+/** This is a React functional component that renders a link to the home page. */
+const FooterContent: React.FC<{ domainAddress: UrlObject }> = ({ domainAddress }) => {
+  return (
+    <div className="flex justify-center">
+      <span className="text-center text-sm dark:text-gray-400">
+        © 2023 <Link href={domainAddress}>Samith K S</Link>. All Rights Reserved.
+      </span>
+    </div>
+  )
+}
+
+/** A React functional component called TheFooter, which returns a footer element in which the FooterContent */
 export default function TheFooter() {
   return (
     <footer>
       <div className="w-full pb-2 pt-6">
-        <div className="flex justify-center">
-          <span className="text-center text-sm dark:text-gray-400">
-            © 2023 <Link href={footerReference}>Samith K S</Link>. All Rights Reserved.
-          </span>
-        </div>
+        <FooterContent domainAddress={footerReference} />
       </div>
     </footer>
   )
