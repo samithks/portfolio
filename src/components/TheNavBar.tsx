f5b326e390b68111dd79f41617ccdf609813db20
Samith K S <samithks@users.noreply.github.com>
2023-05-23 00:07:59 +0530

    local storage

--- src/components/TheNavBar.tsx
+++ src/components/TheNavBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { TbGitPullRequestDraft } from 'react-icons/tb'
+import { useState } from 'react'
 
 import Link from 'next/link'
 
@@ -9,7 +10,7 @@ import TheDarkMode from '@/components/TheDarkMode'
 
 /** This is a React functional component that renders a logo with a link to the home page of the website. */
 function TheLogo() {
-  const currentMode = typeof window !== 'undefined' ? localStorage.getItem('darkMode') === 'true' : null
+  const [currentMode] = useState(typeof window !== 'undefined' ? localStorage.getItem('darkMode') === 'true' : null)
   return (
     <div className="h-12 w-12 flex-shrink-0 rounded-md bg-slate-700 ring-2 ring-black ring-opacity-5 transition duration-300 ease-in-out hover:scale-105 dark:bg-gray-100">
       <Link href="/" className="items-center">
