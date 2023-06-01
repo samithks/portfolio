d2fd9c70a1c0ff26cc7068630cd9688581c1218d
Samith K S <samithks@users.noreply.github.com>
2023-06-01 10:49:22 +0530

    fix: blog
    added blog

---
+++ src/components/blog/BlogContent.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Image from 'next/image'
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import React, { useState, useEffect } from 'react'
+
+interface BlogContentProps {
+  params: {
+    title: string
+    publishedAt: string
+    formattedDate: string
+    code: string
+  }
+}
+
+const BlogContent: React.FC<BlogContentProps> = ({ params }: BlogContentProps) => {
+  const [showMDX, setShowMDX] = useState<boolean>(false)
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setShowMDX(true)
+    }
+  }, [])
+
+  const components = {
+    Image,
+  }
+  const MDXContent = useMDXComponent(params.code)
+  return showMDX ? (
+    <article className="prose mx-auto max-w-xl py-8 dark:prose-invert">
+      <div className="mb-8 text-center">
+        <h1 className="max-w-[650px] text-3xl font-bold">{params.title}</h1>
+        <time dateTime={params.publishedAt} className="mb-1 text-xs text-gray-600">
+          {params.formattedDate}
+        </time>
+      </div>
+      <MDXContent components={components} />
+    </article>
+  ) : null
+}
+
+export default BlogContent
