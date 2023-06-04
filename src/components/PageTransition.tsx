471e006350ae3d0bf4b1bf4fccb854207dfe041a
Samith K S <samithks@users.noreply.github.com>
2023-06-04 23:46:17 +0530

    doc

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -26,6 +26,17 @@ const pageVariants = {
   },
 }
 
+/**
+ * This is a TypeScript React component that wraps its children in a motion section with page transition animations.
+ *
+ * @param - This is a functional component called `PageTransition` that takes in a single parameter as an object with a
+ *   property called `children`. The `children` property is of type `React.ReactNode`, which means it can be any valid
+ *   React node, such as a component, an element, or a string.
+ * @returns A React functional component that takes in a `children` prop of type `React.ReactNode` and returns a
+ *   `motion.section` component from the `framer-motion` library. The `motion.section` component has `initial`,
+ *   `animate`, and `exit` animation states defined by the `pageVariants` object, and a `style` prop that sets the
+ *   height of the section to 100
+ */
 export default function PageTransition({
   children, // will be a page or nested layout
 }: {
