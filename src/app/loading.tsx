0ef76b1ad0367ffc3116fff81f73ff52fe8b210f
Samith K S <samithks@users.noreply.github.com>
2023-06-09 18:18:19 +0530

    doc added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,8 @@
+/**
+ * This is a functional component that returns a string "Loading..." to indicate that the content is being loaded.
+ *
+ * @returns {string} The "Loading..." message.
+ */
 export default function Loading() {
   // Or a custom loading skeleton component
   return 'Loading...'
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,9 @@
-import Link from 'next/link'
-
+/**
+ * This is a functional component that renders a "Not Found" message with a description of the requested resource not
+ * being found.
+ *
+ * @returns {JSX.Element} The "Not Found" message.
+ */
 export default function NotFound() {
   return (
     <div>
