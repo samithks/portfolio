952c4989dbf07f1aa3b993f6537e986c2a87842d
Samith K S <samithks@users.noreply.github.com>
2023-06-09 18:16:20 +0530

    Doc added

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,10 @@
 import { MetadataRoute } from 'next'
 
+/**
+ * This TypeScript function returns metadata for a robots.txt file with a user agent rule, sitemap URL, and host URL.
+ *
+ * @returns {MetadataRoute.Robots} Robots.txt configuration
+ */
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,11 @@
 import { MetadataRoute } from 'next'
 import { allBlogs } from 'contentlayer/generated'
 
+/**
+ * This TypeScript function returns metadata for a sitemap.xml file with a list of URLs and last modified dates.
+ *
+ * @returns {MetadataRoute.Sitemap} Sitemap configuration
+ */
 export default function sitemap(): MetadataRoute.Sitemap {
   const blogs = allBlogs.map((blog) => ({
     url: `https://samithks.website/blog/${blog._raw.flattenedPath}`,
